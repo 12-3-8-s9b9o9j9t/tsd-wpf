a414bb06a693853543b5fec81d206a4cf411eade
yanis <yanis.bouger@gmail.com>
2023-04-03 12:15:31 +0200

    end of task 1

--- Books.cs
+++ Books.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace HomeLibrary
 {
     public class Book
     {
+        private static int IdCount = 0; 
         public int Id { get; private set; }
 
         public string Title { get; set; }
@@ -20,9 +17,10 @@ namespace HomeLibrary
 
         public BookFormat Format { get; set; }
 
-        public Book(int id)
+        public Book()
         {
-            Id = id;
+            IdCount++;
+            Id = IdCount;
 
         }
     }
@@ -38,21 +36,17 @@ namespace HomeLibrary
         {
             return new List<Book>()
             {
-                new Book(1){ Author = "J.K. Rowling", Format = BookFormat.EBook, IsRead = true, Title = "Harry Potter and the Philosopher's Stone", Year=1997},
+                new Book(){ Author = "J.K. Rowling", Format = BookFormat.EBook, IsRead = true, Title = "Harry Potter and the Philosopher's Stone", Year=1997},
 
-                new Book(1)
-                {
-                    Author = "J.K. Rowling", Format = BookFormat.EBook, IsRead = true, Title = "Harry Potter and the Chamber of Secrets",
-                    Year = 1998
-                },
+                new Book(){ Author = "J.K. Rowling", Format = BookFormat.EBook, IsRead = true, Title = "Harry Potter and the Chamber of Secrets", Year = 1998},
 
-                new Book(3){ Author = "J.K. Rowling", Format = BookFormat.PaperBack, IsRead = true, Title = "Harry Potter and the Prisoner of Azkaban", Year = 1999},
+                new Book(){ Author = "J.K. Rowling", Format = BookFormat.PaperBack, IsRead = true, Title = "Harry Potter and the Prisoner of Azkaban", Year = 1999},
 
-                new Book(4){ Author = "Jonathan Swift", Format = BookFormat.PaperBack, IsRead = fale, Title = "Travels into Several Remote Nations of the World. In Four Parts. By Lemuel Gulliver, First a Surgeon, and then a Captain of several Ships", Year=1972},
+                new Book(){ Author = "Jonathan Swift", Format = BookFormat.PaperBack, IsRead = false, Title = "Travels into Several Remote Nations of the World. In Four Parts. By Lemuel Gulliver, First a Surgeon, and then a Captain of several Ships", Year=1972},
 
-                new Book(5){Author = "Wayne Thomas Batson", Format = BookFormat.EBook, IsRead = true, Title = "Isle of Swords", Year = 2007},
+                new Book(){Author = "Wayne Thomas Batson", Format = BookFormat.EBook, IsRead = true, Title = "Isle of Swords", Year = 2007},
 
-                new Book(5){Author = "Louis A. Meyer", Format = BookFormat.EBook, IsRead = true, Title = "Under the Jolly Roger", Year = 200},
+                new Book(){Author = "Louis A. Meyer", Format = BookFormat.EBook, IsRead = true, Title = "Under the Jolly Roger", Year = 2005},
                 
             };
 
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HomeLibrary;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace tsd_wpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private ObservableCollection<Book> books = new(MyBookCollection.GetMyCollection());
+
+        public ObservableCollection<Book> Books
+        {
+            get { return books; }
+            set { books = value; }
+        }
+
+        private Book selectedBook;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public Book SelectedBook
+        {
+            get { return selectedBook; }
+            set {
+                selectedBook = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedBook"));
+            }
+        }
+
+        private void btAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddBook a = new AddBook();
+            a.ShowDialog();
+            if (a.DialogResult == true)
+            {
+                Books.Add(a.NewBook);
+            }
+        }
+
+        private void btDel_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedBook != null)
+            {
+                var res = MessageBox.Show("Are you sure you want to delete this book?", "Delete book", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (res == MessageBoxResult.Yes)
+                {
+                    Books.Remove(SelectedBook);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a book to delete", "Delete book", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
     }
 }
